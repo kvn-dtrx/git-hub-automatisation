55bc310000d327f91073e2690473aa5347c265f3
Kevin Dietrich <kvndtr@icloud.com>
2025-04-26 17:11:10 +0200

    Add some basic scripts

---
+++ scripts/are_notebooks_cleared.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# ---
+# description: Checks whether iPy notebooks contain uncleared cells.
+# ---
+
+failed=0
+
+while IFS='' read -r -d '' notebook; do
+    if [ -n "$(jq '.cells[] | select(.outputs | length > 0)' "$notebook")" ]; then
+        echo "The notebook ${notebook} contains uncleared output cells and should not be committed!"
+        failed=1
+    fi
+    # It must be ensured that the while loop runs in the main shell,
+    # otherwise changes to the failed variable would be restricted to subshells!.
+done < <(find . -name "*.ipynb" -print0)
+
+if [ "${failed}" -gt 0 ]; then
+    exit 1
+else
+    echo "No uncleared output cells found."
+fi
---
+++ scripts/pushes_to_protected_branch.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# ---
+# description: Checks whether the push operation targets a protected branch.
+# ---
+
+pb="${pb} main"
+pb="${pb} master"
+pb="${pb} untouchable"
+pb="${pb} infallible"
+
+protected_branches="${pb}"
+current_branch="$(git rev-parse --abbrev-ref HEAD)"
+
+for protected_branch in ${protected_branches}; do
+    if [ "${current_branch}" = "${protected_branch}" ]; then
+        echo "Push to ${current_branch} is very like not what you want!"
+        exit 1
+    fi
+done
