d53079c72b3e3c210ff09e37929c9a86c53947d3
Kevin Dietrich <kvndtr@icloud.com>
2025-04-29 22:04:08 +0200

    Temporary commit at 2025-04-29T22:04:08, please squash

--- scripts/are_filenames_formatted.bash
+++ scripts/are_filenames_formatted.bash
@@ -6,14 +6,15 @@
 
 failed=0
 
-while IFS='' read -r -d '' file; do
-    if [[ "${file}" =~ " " ]]; then
-        echo "File name with white spaces found: '${file}'."
-        failed=1
-    fi
-    # It must be ensured that the while loop runs in the main shell,
-    # otherwise changes to the failed variable would be restricted to subshells!.
-done < <(git diff --cached --name-only -z)
+echo $failed
+# while IFS='' read -r -d '' file; do
+#     if [[ "${file}" =~ " " ]]; then
+#         echo "File name with white spaces found: '${file}'."
+#         failed=1
+#     fi
+#     # It must be ensured that the while loop runs in the main shell,
+#     # otherwise changes to the failed variable would be restricted to subshells!.
+# done < <(git diff --cached --name-only -z)
 
 if [ "${failed}" -gt 0 ]; then
     exit 1
